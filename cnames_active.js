a8bcd21b5ef14c0179bd3b837ee6450aa6848243
samdiedddd <176967927+samdied@users.noreply.github.com>
2026-03-30 03:36:48 +0530

    chordsbuddy.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -544,6 +544,7 @@ var cnames_active = {
   "chitchat": "chitchatjs.github.io",
   "choo": "choo-js.github.io",
   "chordthing": "ifons42.github.io/chordthing",
+  "chordsbuddy" "samdied.github.io/chordsbuddy/",
   "chris": "christopher-hayes.github.io/chris",
   "chrismendis": "chrismendis.github.io", // noCF? (don´t add this in a new PR)
   "christo": "christoga.github.io/js-org", // noCF? (don´t add this in a new PR)
